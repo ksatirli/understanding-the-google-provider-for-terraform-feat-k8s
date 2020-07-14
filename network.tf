81ac9a7616ee0fb96ae8bd3d5a0149cec9bf05cc
Kerim Satirli <k.satirli@gmail.com>
2020-07-14 11:27:40 +0200

    terraform fmt

--- network.tf
+++ network.tf
@@ -10,8 +10,7 @@ module "hug_ist_gke_network" {
       subnet_name   = "${var.project_prefix}-subnet-01"
       subnet_ip     = "10.10.10.0/24"
       subnet_region = var.project_region
-    },
-    {
+      }, {
       subnet_name           = "${var.project_prefix}-subnet-02"
       subnet_ip             = "10.10.20.0/24"
       subnet_region         = var.project_region
