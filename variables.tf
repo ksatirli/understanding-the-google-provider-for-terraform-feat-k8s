b0bccff44e262ba1115bda1a86008206798d54a2
Kerim Satirli <k.satirli@gmail.com>
2020-07-14 11:42:44 +0200

    adds local variables and related outputs to allow for better code understanding

--- variables.tf
+++ variables.tf
@@ -21,6 +21,23 @@ variable "project_prefix" {
   description = "Project Prefix"
   default     = "hug-ist-demo"
 }
+
+locals {
+  subnetwork_names = keys(module.hug_ist_gke_network.subnets)
+
+  # use the lookup function to find the key at index 0 of `subnet_names` (see previous line),
+  # then use this key to retrieve the data of that specific subnet from `module.hug_ist_gke_network.subnets`
+
+  # Please note: While this approach is entirely codified, it is not necessarily the easiest code to read or maintain;
+  # if you want to understand this better, check out `outputs.tf`. Uncomment all `outputs` and run `terraform apply`.
+  # The outputs are built to take you step-by-step from the first value to the last one, as shown in the next four lines:
+  subnetwork_services_name          = lookup(lookup(module.hug_ist_gke_network.subnets, local.subnetwork_names[0], null), "name", null)
+  subnetwork_services_ip_cidr_range = lookup(lookup(module.hug_ist_gke_network.subnets, local.subnetwork_names[0], null), "ip_cidr_range", null)
+
+  subnetwork_pods_name          = lookup(lookup(module.hug_ist_gke_network.subnets, local.subnetwork_names[1], null), "name", null)
+  subnetwork_pods_ip_cidr_range = lookup(lookup(module.hug_ist_gke_network.subnets, local.subnetwork_names[1], null), "ip_cidr_range", null)
+}
+
 # get your public IP address by querying ICHI
 data "http" "icanhazip" {
   # this value will be available in data.http.icanhazip.body
