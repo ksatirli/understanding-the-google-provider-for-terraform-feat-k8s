3b2476743a59bc599684db6e42f25ed4814e2ce4
Taylor Dolezal <onlydole@gmail.com>
2020-07-16 07:11:16 -0700

    Rename yaml folder to kubernetes

--- kubernetes.tf
+++ kubernetes.tf
@@ -63,7 +63,3 @@ resource "kubernetes_service" "beacon" {
     type = "LoadBalancer"
   }
 }
-
-output "beacon_url" {
-  value = "${kubernetes_service.beacon.load_balancer_ingress.0.ip}:${kubernetes_service.beacon.spec.0.port.0.port}"
-}
