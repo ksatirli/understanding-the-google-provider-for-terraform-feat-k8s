c028cca2132f5ed7e525affca1eb91ab778bc65d
Taylor Dolezal <onlydole@gmail.com>
2020-07-27 19:10:13 -0700

    Administrivia updates

--- terraform.tf
+++ terraform.tf
@@ -1,11 +1,11 @@
 terraform {
   required_providers {
-    google      = "~> 3.16.0"
-    google-beta = "~> 3.29.0"
-    http        = "~> 1.2.0"
-    kubernetes  = "~> 1.11.3"
-    null        = "~> 2.1.2"
+    google      = "~> 3.16"
+    google-beta = "~> 3.29"
+    http        = "~> 1.2"
+    kubernetes  = "~> 1.11"
+    null        = "~> 2.1"
   }
 
-  required_version = "~> 0.12.28"
+  required_version = "~> 0.12"
 }
